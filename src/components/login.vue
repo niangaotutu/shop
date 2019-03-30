0d592534cdcf06154b9861c343cacc876d354af8
niangaotutu <519590370@qq.com>
2019-03-30 17:39:42 +0800

    登陆组件

---
+++ src/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-wrap">
+      <h2>用户登录</h2>
+    <el-form class="login-form"   :label-position="labelPosition" label-width="80px" :model="formdata">
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+     
+    </el-form>
+     <el-button  class="login-btn" type="success">成功按钮</el-button>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+          labelPosition: 'top',
+          formdata: {
+          name: '',
+          password: '',
+        }
+    }
+}};
+</script>
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from "@/components/login.vue"
+import login from "../components/login.vue"
 Vue.use(Router)
 
 export default new Router({
