9db57a1e6f06112b0a3b544514db28b6529288af
niangaotutu <519590370@qq.com>
2019-03-30 21:29:05 +0800

    侧边栏调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,89 +1,118 @@
 <template>
+  <el-container class="container">
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt>
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href class="exit">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">
+        <el-menu  class="el-menu-vertical-demo" :unique-opened="true" :router='true'>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-info"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
 
-    <el-container class="container">
-      <el-header>
-          <el-row>
-  <el-col :span="4">
-      <img src="../assets/logo.png" alt="">
-  </el-col>
-  <el-col :span="19" class="middle"><h2>电商后台管理系统</h2></el-col>
-  <el-col :span="1"><a href=""  class="exit">退出</a></el-col>
-</el-row>
-      </el-header>
-      <el-container>
-        <el-aside  class="aside" width="200px">
-            <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
-        </el-aside>
-        <el-main   class="main">Main</el-main>
-      </el-container>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-sort-down"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+              <el-menu-item index="1-1">
+              <i class="el-icon-star-on"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-service"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+             <el-menu-item index="1-1">
+              <i class="el-icon-service"></i>
+               <span>分类参数</span>
+            </el-menu-item>
+             <el-menu-item index="1-1">
+              <i class="el-icon-service"></i>
+               <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+             <el-menu-item index="1-1">
+              <i class="el-icon-info"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+           <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+             <el-menu-item index="1-1">
+              <i class="el-icon-info"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
     </el-container>
- 
+  </el-container>
 </template>
 <script>
 export default {
-    data(){
-
-    return {
-        
-    }
-    }
+  data() {
+    return {};
+  }
 };
 </script>
 <style>
-.container{
-    background-color: #b3c0d1;
-    height: 100%;
+.container {
+  background-color: #b3c0d1;
+  height: 100%;
 }
 /* .aside{
 } */
-.main{
-    background-color: red;
+.main {
+  background-color: red;
 }
-.middle{
-    line-height: 60px;
-    text-align: center;
-    color:white;
+.middle {
+  line-height: 60px;
+  text-align: center;
+  color: white;
 }
-.exit{
-     line-height: 60px;
-    text-align: center;
-    text-decoration: none
+.exit {
+  line-height: 60px;
+  text-align: center;
+  text-decoration: none;
 }
-
-
 </style>
