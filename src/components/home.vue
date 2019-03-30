f93dddfa2307cf98b5f5d6e939114fef4276f699
niangaotutu <519590370@qq.com>
2019-03-30 19:57:42 +0800

    home组件布局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>home </div>
+
+    <el-container class="container">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside  class="aside"    width="200px">Aside</el-aside>
+        <el-main   class="main">Main</el-main>
+      </el-container>
+    </el-container>
+ 
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style>
+.container{
+    background-color: pink;
+    height: 100%;
+}
+.aside{
+    background-color: blue;
+}
+.main{
+    background-color: red;
+}
 
 </style>
